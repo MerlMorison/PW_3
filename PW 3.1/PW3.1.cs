f5e9ecea2dff1c8d77ab2bf8bf0fd326b46bbe51
MerlMorison <merlmorison@gmail.com>
2024-03-17 10:58:31 +0200

    Added PW_3.1

---
+++ PW 3.1/PW3.1.cs
@@ -0,0 +1,17 @@
+﻿namespace PW_3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter the number: ");
+            int value = int.Parse(Console.ReadLine());
+            if (value > 0)
+                Console.WriteLine($"Number {value} is positive");
+            else if (value < 0)
+                Console.WriteLine($"Number {value} is negative");
+            else
+                Console.WriteLine($"Number {value} is 0");
+        }
+    }
+}
