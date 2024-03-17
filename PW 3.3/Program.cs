7aa0adff487c3fa978f6a40ce4c63f351c024b03
MerlMorison <merlmorison@gmail.com>
2024-03-17 10:59:43 +0200

    Added PW_3.3

---
+++ PW 3.3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PW_3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter the first number: ");
+            var value1 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter the second number: ");
+            var value2 = int.Parse(Console.ReadLine());
+
+            var result = value1 < value2 ? ($"{value2} is bigger") : ($"{value1} is bigger");
+
+            Console.WriteLine(result);
+        }
+    }
+}
