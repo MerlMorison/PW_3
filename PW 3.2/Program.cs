59adcebf8547cbfb01b96cb082344d477e767638
MerlMorison <merlmorison@gmail.com>
2024-03-17 10:59:16 +0200

    Added PW_3.2

---
+++ PW 3.2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PW_3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter the number: ");
+            var value = int.Parse(Console.ReadLine());
+
+            if (value >= 10 && value <= 20)
+            {
+                Console.WriteLine("value >= 10 and value <= 20");
+            }
+            else if (value >= 30 && value <= 40)
+            {
+                Console.WriteLine("value >= 30 and value <= 40");
+            }
+            else
+            {
+                Console.WriteLine($"Enother value");
+            }
+        }
+    }
+}
